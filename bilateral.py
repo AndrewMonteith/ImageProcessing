41ac647e53a2878965d1858728115f42053a0fac
AndrewMonteith <andrewmonteith1999@gmail.com>
2018-11-20 00:11:56 +0000

    Inital Commit

---
+++ bilateral.py
@@ -0,0 +1,98 @@
+'''
+    Filename: bilaterial.py
+    Purpose: Applies a bilateral filter onto an image
+'''
+
+import cv2
+import numpy as np
+from math import exp, sqrt, pi
+
+# ----------------- / Constants
+INPUT_IMAGE = 'GoodBoy.png'
+
+# ------------------ / Filtering
+
+
+def createBlank(width, height, rgb_color=(0, 0, 0)):
+    """Create new image(numpy array) filled with certain color in RGB"""
+    # Create black blank image
+    image = np.zeros((height, width, 3), np.uint8)
+
+    # Since OpenCV uses BGR, convert the color first
+    color = tuple(reversed(rgb_color))
+    # Fill image with color
+    image[:] = color
+
+    return image
+
+
+def neighbourhood(x, y, width, height, kernelSize):
+    kernelOffset = 0.5 * (kernelSize - 1)
+
+    minX = int(max(0, x - kernelOffset))
+    maxX = int(min(x + kernelOffset, width - 1))
+    minY = int(max(y - kernelOffset, 0))
+    maxY = int(min(y + kernelOffset, height - 1))
+
+    for X in range(minX, maxX):
+        for Y in range(minY, maxY):
+            yield (X, Y)
+
+
+def createGauss(sigma):
+    k = 1 / (sigma * sqrt(2 * pi))
+    K = 1 / ((-2) * sigma**2)
+
+    return lambda x: k * np.exp(x**2 * K)
+
+
+def bilaterial_filter(img, sigmaDistance, sigmaIntensity, kernelSize=5):
+    gaussDist = createGauss(sigmaDistance)
+    gaussIntensity = createGauss(sigmaIntensity)
+
+    (height, width, _) = img.shape
+
+    def process_pixel(x, y):
+        newB, newG, newR = 0, 0, 0
+        normB, normG, normR = 0, 0, 0
+
+        (origB, origG, origR) = img[y, x]
+
+        for (nX, nY) in neighbourhood(x, y, width, height, kernelSize):
+            (b, g, r) = img[nY, nX]
+
+            # Computing Coefficients
+            distCoef = gaussDist(sqrt((x - nX)**2 + (y - nY)**2))
+            coefB = distCoef * gaussIntensity(origB - b)
+            coefG = distCoef * gaussIntensity(origG - g)
+            coefR = distCoef * gaussIntensity(origR - r)
+
+            normB += coefB
+            normG += coefG
+            normR += coefR
+
+            newB += (coefB * b)
+            newG += (coefG * g)
+            newR += (coefR * r)
+
+        return (newB / normB, newG / normG, newR / normR)
+
+    result = createBlank(width, height)
+
+    for y in range(0, height):
+        for x in range(0, width):
+            result[y, x] = process_pixel(x, y)
+
+    return result
+
+
+    # ------------------ / Runner
+if __name__ == "__main__":
+    img = cv2.imread(INPUT_IMAGE, cv2.IMREAD_COLOR)
+
+    if img is not None:
+        filtered = bilaterial_filter(img, sigmaDistance=3, sigmaIntensity=80.0)
+
+        cv2.imwrite('filtered.png', filtered)
+    else:
+        print("Couldn't find image " + INPUT_IMAGE)
---
+++ gaussian.py
@@ -0,0 +1,83 @@
+'''
+    Filename: gaussian.py
+    Description: Applies a guassian filter to a given image
+    Note: Alongside this file is an example blurred with the inbuit
+          opencv so you can check whether the filter works.
+'''
+import cv2
+from math import sqrt, exp, pi
+import numpy as np
+
+# --------- / Constants
+IMAGE_NAME = 'goodboy.jpg'
+
+# --------- / Custom Blurring
+
+
+def create_blank(width, height, rgb_color=(0, 0, 0)):
+    """Create new image(numpy array) filled with certain color in RGB"""
+    # Create black blank image
+    image = np.zeros((height, width, 3), np.uint8)
+
+    # Since OpenCV uses BGR, convert the color first
+    color = tuple(reversed(rgb_color))
+    # Fill image with color
+    image[:] = color
+
+    return image
+
+
+def neighbourhood(x, y, width, height, kernelSize):
+    kernelOffset = 0.5 * (kernelSize - 1)
+
+    minX = int(max(0, x - kernelOffset))
+    maxX = int(min(x + kernelOffset, width - 1))
+    minY = int(max(y - kernelOffset, 0))
+    maxY = int(min(y + kernelOffset, height - 1))
+
+    for X in range(minX, maxX):
+        for Y in range(minY, maxY):
+            yield (X, Y)
+
+
+def gaussian_blur(img, kernelSize, sigma):
+    k, K = 1 / (sqrt(2 * pi) * sigma), 1 / ((-2) * sigma**2)
+
+    def gauss(x):
+        return k * exp((x * x) * K)
+
+    (height, width, _) = img.shape
+    result = create_blank(width, height)
+
+    def process_pixel(x, y):
+        newR, newG, newB = 0, 0, 0
+        normal = 0
+
+        for (nX, nY) in neighbourhood(x, y, width, height, kernelSize):
+            (b, g, r) = img[nY, nX]
+            coef = gauss(sqrt((x - nX)**2 + (y - nY)**2))
+
+            newR += (r * coef)
+            newG += (g * coef)
+            newB += (b * coef)
+            normal += coef
+
+        return (newB / normal, newG / normal, newR / normal)
+
+    for y in range(0, height):
+        print("Doing Row:", y)
+        for x in range(0, width):
+            result[y, x] = process_pixel(x, y)
+
+    return result
+
+# --------- / Entry Point
+
+
+if __name__ == "__main__":
+    img = cv2.imread(IMAGE_NAME, cv2.IMREAD_COLOR)
+
+    if img is not None:
+
+        blurred = gaussian_blur(img, kernelSize=7, sigma=80)
+        cv2.imwrite(IMAGE_NAME + "filtered.jpg", blurred)
