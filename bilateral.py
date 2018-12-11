5141b1d581000804380106d595b3cecb42ea818f
AndrewMonteith <andrewmonteith1999@gmail.com>
2018-12-11 16:08:02 +0000

    comments, comments, comments.

--- bilateral.py
+++ bilateral.py
@@ -10,24 +10,18 @@ from math import exp, sqrt, pi, e
 # ----------------- / Constants
 INPUT_NON_FLASH = 'test3a.jpg'
 INPUT_FLASH = 'test3b.jpg'
+FILTERED_IMAGE_NAME = 'result.jpg'
 
 # ------------------ / Filtering
 
 
-def createBlank(width, height, channels, rgb_color=(0, 0, 0)):
-    """Create new image(numpy array) filled with certain color in RGB"""
-    # Create black blank image
-    image = np.zeros((height, width, channels), np.uint8)
-
-    # Since OpenCV uses BGR, convert the color first
-    color = tuple(reversed(rgb_color))
-    # Fill image with color
-    image[:] = color
-
-    return image
+def createBlankImage(width, height, channels):
+    """Create new image(numpy array)"""
+    return np.zeros((height, width, channels), np.uint8)
 
 
 def neighbourhood(x, y, width, height, kernelSize):
+    "Generator that returns all neighbourhood coordinates for a given pixel"
     kernelOffset = 0.5 * (kernelSize - 1)
 
     minX = int(max(0, x - kernelOffset))
@@ -41,6 +35,7 @@ def neighbourhood(x, y, width, height, kernelSize):
 
 
 def createGauss(sigma):
+    "Constructs a Gaussian Function for a given sigma"
     k = 1 / (sigma * sqrt(2 * pi))
     K = 1 / ((-2) * sigma**2)
 
@@ -48,42 +43,45 @@ def createGauss(sigma):
 
 
 def joint_bilateral_filter(imgNonFlash, imgFlash, sigmaDistance, sigmaIntensity, kernelSize=5):
+    "Applies Joint Bilateral Filter to image pair (imageNonFlash, imageFlash)"
+
     gaussDist = createGauss(sigmaDistance)
     gaussIntensity = createGauss(sigmaIntensity)
 
     (height, width, channels) = imgNonFlash.shape
 
     intensityCoefficients = [gaussIntensity(x) for x in range(0, 256)]
-    
+
     def process_pixel(x, y):
-        newColours = [0]*channels
-        normalisationFactors = [0]*channels
+        "Computes new colour vector for each pixel (x, y)"
+
+        newColourVector = [0] * channels
+        normalisationFactors = [0] * channels
+        coefficients = [0] * channels
 
         centerFlashVector = imgFlash[y, x]
 
-        coefficients = [0] * channels
         for (nX, nY) in neighbourhood(x, y, width, height, kernelSize):
             neighbourNonFlashVector = imgNonFlash[nY, nX]
             neighbourFlashVector = imgFlash[nY, nX]
 
-            distCoef = gaussDist(sqrt((x - nX)**2 + (y - nY)**2))
+            distanceCoefficient = gaussDist(sqrt((x - nX)**2 + (y - nY)**2))
 
             for i in range(0, channels):
-                coefficient = distCoef * intensityCoefficients[abs(neighbourFlashVector[i] - centerFlashVector[i])]
-                coefficients[i] = coefficient 
+                coefficient = distanceCoefficient * intensityCoefficients[abs(neighbourFlashVector[i] - centerFlashVector[i])]
+                coefficients[i] = coefficient
                 normalisationFactors[i] += coefficient
-                newColours[i] += coefficient * neighbourNonFlashVector[i]
+                newColourVector[i] += coefficient * neighbourNonFlashVector[i]
 
-
+        # Normalise Each Colour.
         for i in range(0, channels):
-            newColours[i] = newColours[i] / normalisationFactors[i]
-        
-        return newColours
-        
-    result = createBlank(width, height, channels)
+            newColourVector[i] = newColourVector[i] / normalisationFactors[i]
+
+        return newColourVector
+
+    result = createBlankImage(width, height, channels)
 
     for y in range(0, height):
-        print(y, height)
         for x in range(0, width):
             result[y, x] = process_pixel(x, y)
 
@@ -94,13 +92,14 @@ if __name__ == "__main__":
     imgFlash = cv2.imread(INPUT_FLASH, cv2.IMREAD_UNCHANGED)
     imgNoFlash = cv2.imread(INPUT_NON_FLASH, cv2.IMREAD_UNCHANGED)
 
-    if imgFlash is not None:
-        sigmaIntensity = 40
-        sigmaDistance = 0.1
+    if (imgFlash is not None) and (imgNoFlash is not None):
+        sigmaIntensity = 20
+        sigmaDistance = 1.8
+        kernelSize = 11
 
         filtered = joint_bilateral_filter(
-            imgNoFlash, imgFlash, sigmaDistance, sigmaIntensity, kernelSize=5)
+            imgNoFlash, imgFlash, sigmaDistance, sigmaIntensity, kernelSize)
 
-        cv2.imwrite('result_lower_sigma.jpg', filtered)
+        cv2.imwrite(FILTERED_IMAGE_NAME, filtered)
     else:
-        print("Couldn't find image " + INPUT_FLASH)
+        print("Couldn't find images")
